a7d1e3c757343b4578af98f2101c271564e1758a
end0cr1ne <f.warren210@gmail.com>
2016-01-16 23:52:58 +0530

    debug

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var ExpressPeerServer = require('peer').ExpressPeerServer;
 
 server.listen(process.env.PORT || 80);
 
 io.peers=[];
+var options = {debug: true};
 
 app.use(express.static('app'));
 app.use('/bower_components', express.static('bower_components'));
+app.use('/peerjs', ExpressPeerServer(server, options));
 
 io.on('connection', function (socket) {
+    socket.emit('setup',process.env.PORT);
     socket.on('new', function (data) {
         socket.emit('peers',io.peers);
         io.peers.push({name:data.name, id:data.id, sid:socket.id});
