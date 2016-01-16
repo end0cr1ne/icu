1ee922191f96312c59d5f9e674af9005d2e89695
end0cr1ne <f.warren210@gmail.com>
2016-01-16 21:04:02 +0530

    debug

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ io.peers=[];
 
 app.use(express.static('app'));
 app.use('/bower_components', express.static('bower_components'));
-app.get('/',function(req,res){
-    res.sendFile('app/index.html');
-});
 
 io.on('connection', function (socket) {
     socket.on('new', function (data) {
