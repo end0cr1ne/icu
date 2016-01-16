d649dd829d79053fe3ed9482dbd3d42bfc3508a1
end0cr1ne <f.warren210@gmail.com>
2016-01-16 19:40:52 +0530

    debug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(process.env.PORT || 8086);
 
 io.peers=[];
 
